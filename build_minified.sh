93ebde5c1d06dced0be624d287e5448cf61051ea
Karel Wintersky <karel.wintersky@yandex.ru>
2017-09-21 12:07:15 +0300

    + examples and demo version moded to ./dest directory * fixed build_minified.sh

--- build_minified.sh
+++ build_minified.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 # Generating minimized file (all-in-one)
+DIR_SOURCES='./'
+DIR_DEST='./dest'
 
 echo 'Generating all-in-one php-file'
 
-cp generate.php sitemap_generator.php
+cp $DIR_SOURCES/generate.php $DIR_DEST/sitemap_generator.php
 
-# sed -i -e "/??/{r FILE"  -e 'd}' sitemap_generator.php
-
-echo 'core.sitemapgen.php'
-cp core.sitemapgen.php __tmp__.php
+echo 'Incapsulating core.sitemapgen.php'
+cp $DIR_SOURCES/core.sitemapgen.php __tmp__.php
 sed -i "1s/.*/\/\*\*\//" __tmp__.php
-sed -i -e "/core\.sitemapgen\.php/{r __tmp__.php"  -e ' d}' sitemap_generator.php
+sed -i -e "/core\.sitemapgen\.php/{r __tmp__.php"  -e ' d}' $DIR_DEST/sitemap_generator.php
 
-echo 'class.INI_config.php'
-cp class.INI_config.php __tmp__.php
+echo 'Incapsulating class.INI_config.php'
+cp $DIR_SOURCES/class.INI_config.php __tmp__.php
 sed -i "1s/.*/\/\*\*\//" __tmp__.php
-sed -i -e "/class\.INI_config\.php/{r __tmp__.php"  -e 'd}' sitemap_generator.php
+sed -i -e "/class\.INI_config\.php/{r __tmp__.php"  -e 'd}' $DIR_DEST/sitemap_generator.php
 
-echo 'class.DBConnectionLite.php'
-cp class.DBConnectionLite.php __tmp__.php
+echo 'Incapsulating class.DBConnectionLite.php'
+cp $DIR_SOURCES/class.DBConnectionLite.php __tmp__.php
 sed -i "1s/.*/\/\*\*\//" __tmp__.php
-sed -i -e "/class\.DBConnectionLite\.php/{r __tmp__.php"  -e 'd}' sitemap_generator.php
+sed -i -e "/class\.DBConnectionLite\.php/{r __tmp__.php"  -e 'd}' $DIR_DEST/sitemap_generator.php
 
-echo 'class.sitemap_file_saver.php'
-cp class.sitemap_file_saver.php __tmp__.php
+echo 'Incapsulating class.sitemap_file_saver.php'
+cp $DIR_SOURCES/class.sitemap_file_saver.php __tmp__.php
 sed -i "1s/.*/\/\*\*\//" __tmp__.php
-sed -i -e "/class\.sitemap_file_saver\.php/{r __tmp__.php"  -e 'd}' sitemap_generator.php
+sed -i -e "/class\.sitemap_file_saver\.php/{r __tmp__.php"  -e 'd}' $DIR_DEST/sitemap_generator.php
 
 rm -f __tmp__.php
 
 echo 'Ok.'
-echo 'sitemap_generator.php GENERATED'
+echo "$DIR_DEST/sitemap_generator.php GENERATED"
